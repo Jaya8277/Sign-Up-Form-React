6ae86d83e927aa62e1d75a75288a4112a34be271
Jaya8277 <jayavish423@gmail.com>
2022-05-15 00:49:40 +0530

    Sign-up-form-react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import MultipelInputs from '../src/components/forms/multipelInputs';
+
+function App() {
+  return (
+    <>
+      <MultipelInputs/>
+    </>
+  );
+}
+
+export default App;
